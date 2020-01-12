6110dc304e8bdb03afff9e40339ee9c2d6913834
ScruffyLNH <anonymousandy829@gmail.com>
2020-01-12 14:59:15 +0100

    Implement bare bones bot

--- pyEBOT.py
+++ pyEBOT.py
@@ -3,11 +3,14 @@ from discord.ext import commands
 
 client = commands.Bot('!')
 
-print('hello')
-
 
 @client.event
 async def on_ready():
     print('Ready.')
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
 
 client.run('NjY1ODQ2MTI2NzMzNDI2NzA5.Xhrj1Q.dWY00Kdjl8jTsVx4K099PCc-4QY')

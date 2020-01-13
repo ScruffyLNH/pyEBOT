a7c34fb93c4d535a1b9098510da854ec8e6fd054
ScruffyLNH <anonymousandy829@gmail.com>
2020-01-13 05:14:28 +0100

    Removed commands that will be put in cogs

--- pyEBOT.py
+++ pyEBOT.py
@@ -1,16 +1,28 @@
-import discord
+import discord # noqa
+import os
 from discord.ext import commands
 
 client = commands.Bot('!')
 
 
-@client.event
-async def on_ready():
-    print('Ready.')
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
 
 
 @client.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+
+@client.command()
+async def reload(ctx, extension):
+    client.reload_extension(f'cogs.{extension}')
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
 
 client.run('NjY1ODQ2MTI2NzMzNDI2NzA5.Xhrj1Q.dWY00Kdjl8jTsVx4K099PCc-4QY')

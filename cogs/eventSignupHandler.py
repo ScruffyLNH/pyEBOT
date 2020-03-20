2c8a619d10a62cefbf3bf554cffa6aeb6df333e1
ScruffyLNH <anonymousandy829@gmail.com>
2020-03-20 23:48:23 +0100

    Break up checks to smaller methods

--- cogs/eventSignupHandler.py
+++ cogs/eventSignupHandler.py
@@ -45,109 +45,7 @@ class EventSignupHandler(commands.Cog):
 
         messageId = payload.message_id
         # Check which event was reacted to
-        orgEvent = self.findEvent(messageId)
-        # Get the Channel object.
-        channel = self.client.get_channel(payload.channel_id)
-        # Get the Message object.
-        message = await channel.fetch_message(messageId)
-        # Get the Guild object.
-        guild = message.guild
-        # Get the user who reacted
-        member = guild.get_member(payload.user_id)
-        # Get the role that official members will have.
-        memberRole = guild.get_role(Constants.MEMBER_ROLE_ID)
-        # Get current utc time
-        currentTime = datetime.utcnow()
-
-        if orgEvent and not member.bot:
-            # Check if event has passed.
-            if currentTime > orgEvent.data['Date Time']:
-                await member.send(
-                    'Sorry, you\'d need a time machine to join this event.',
-                    delete_after=60.0
-                    )
-                await message.remove_reaction('✅', member)
-                return
-
-            # Check if within deadline.
-            if orgEvent.data['Deadline'] != '':
-                if currentTime > orgEvent.data['Deadline']:
-                    await member.send(
-                        'Sorry, signup deadline has passed.',
-                        delete_after=60.0
-                    )
-                    await message.remove_reaction('✅', member)
-                    return
-
-            # If event is private, check if user has proper authorization.
-            if orgEvent.data['Members Only'].upper() == 'YES':
-                if memberRole not in member.roles:
-                    await member.send(
-                        'Sorry, this event is for members only.',
-                        delete_after=60.0
-                    )
-                    await message.remove_reaction('✅', member)
-                    return
-
-            # Check if user already exists in the Event object.
-            _user = orgEvent.getParticipant(member.id)  # TODO: Make this check for the acual role instead of person to determine if user is already signed up.
-            if _user:
-                # Check if user already has the role.
-                r = _user.getRole(orgEvent.roles['participant'].id)
-                if r:
-                    await member.send(
-                        'Hmm, you seem to already be registered for the event.'
-                        ' Strange. :shrug:',
-                        delete_after=60.0
-                    )
-                    return
-
-            # If user has passed all flags, add user to event and update
-            # the embed.
-            p = event.Person(member.id, member.name)
-            if not p.getRole(orgEvent.roles['participant'].id):
-                r = event.Role(memberRole.name, memberRole.id)
-                p.roles.append(r)  # TODO: Change persons roles to list
-                orgEvent.participants.append(p)
-
-                # TODO: Update the embed.
-
-            # If event has separate channel add roles to user, and send a
-            # welcome mention in the text channel.
-            if orgEvent.roles:
-                participantRole = guild.get_role(
-                    orgEvent.roles['participant'].id
-                )
-                if participantRole not in member.roles:
-                    await member.add_roles(participantRole)
-
-                # TODO: Refactor. Make reference to orgEvent channel more robust.
-                eventChannel = self.client.get_channel(
-                    orgEvent.channels[0].id
-                )
-                welcomeMsg = (
-                    f'Welcome to the event {member.mention}, '
-                    'glad to have you!'
-                )
-                await eventChannel.send(welcomeMsg, delete_after=60.0)
-        elif member.bot:
-            pass
-        else:
-            print(
-                'Someone tried to sign up for an event that no longer exists.'
-            )
-
-        # If private event check if user is member.
-        # Instanciate user and add to client.orgEvents.
-        # Update embed.
-        # Grant user roles if the event has a private channel.
-        # Mention user in event channel: "@examplePerson Welcome to the event!"
-
-    async def handleCancellation(self, payload):  # TODO: generalize method to handle more emojis and roles.
-
-        messageId = payload.message_id
-        # Check which event was reacted to
-        orgEvent = self.findEvent(messageId)
+        orgEvent = self.getEvent(messageId)
         # Get channel object
         channel = self.client.get_channel(payload.channel_id)
         # Get the Message object.
@@ -166,7 +64,177 @@ class EventSignupHandler(commands.Cog):
             if person:
                 person.removeRole(roleId)
 
-    def findEvent(self, id):
+    async def handleParticipationRequest(self, payload):
+        """
+        messageId = payload.message_id
+        # Check which event was reacted to
+        orgEvent = self.getEvent(messageId)
+        # Get the Channel object.
+        channel = self.client.get_channel(payload.channel_id)
+        # Get the Message object.
+        message = await channel.fetch_message(messageId)
+        # Get the Guild object.
+        guild = message.guild
+        # Get the user who reacted
+        member = guild.get_member(payload.user_id)
+        # Get the role that official members will have.
+        memberRole = guild.get_role(Constants.MEMBER_ROLE_ID)
+        """
+
+        data = await self.gatherPayloadData(payload)
+        orgEvent = data[0]
+        member = data[1]
+
+        # No action should be taken if the bot made the reaction.
+        if member.bot:
+            return
+
+        # Check that event was found in the internal record.
+        if not orgEvent:
+            print('Someone tried to sign up for an untracked event.')
+            return
+
+        # Check that event is not over, and handle rejection if required.
+        if await self.eventHasPassed(data):
+            return
+
+        # Check that the deadline has not passed, and handle rejection if req.
+        if await self.deadlineExceeded(data):
+            return
+
+        # If event is private, check if user has proper authorization.
+        if orgEvent.data['Members Only'].upper() == 'YES':
+            # Check that user has member role, handle rejection if not.
+            if await self.noMemberRole(data):
+                return
+
+        # Attempt to get person object
+        person = orgEvent.getParticipant(member.id)
+        if person is not None:
+            # Check if user already has role, handle rejection if necessary.
+            if await self.userAlreadyHasRole(data):
+                return
+
+        # If user has passed all flags, add user to event and update
+        # the embed.
+
+        # Add role/person with role to internal event object.
+        if person is None:
+            person = event.Person(member.id, member.name)
+            person.roles.append(orgEvent.roles['participant'])
+            orgEvent.participants.append(person)
+        else:
+            person.roles.append(orgEvent.roles['participant'])
+        # TODO: Serialize data here.
+
+        # Add discord role.
+        await self.addDiscordRole(member, orgEvent.roles['participant'])
+
+        # Print welcome message to the discussion channel.
+        await self.sendWelcomeMsg(member, orgEvent.channels['discussion'])
+
+        # TODO: Update the embed.
+
+    async def eventHasPassed(self, data):
+        # TODO: docstring.
+
+        # Gather necessary data from the payload.
+        orgEvent, member, message, memeberRole, emoji = data
+
+        # Check if event has passed.
+        currentTime = datetime.utcnow()
+        if currentTime > orgEvent.data['Date Time']:
+            await member.send(
+                'Sorry, you\'d need a time machine to join this event.',
+                delete_after=60.0
+                )
+            await message.remove_reaction(emoji, member)
+            return True
+        return False
+
+    async def deadlineExceeded(self, data):
+        # TODO: Doctring...
+
+        # Gather necessary data from the payload.
+        orgEvent, member, message, memberRole, emoji = data
+
+        # Check if deadline has passed.
+        currentTime = datetime.utcnow()
+        if orgEvent.data['Deadline'] != '':
+            if currentTime > orgEvent.data['Deadline']:
+                await member.send(
+                    'Sorry, signup deadline has passed.',
+                    delete_after=60.0
+                )
+                await message.remove_reaction(emoji, member)
+                return True
+        return False
+
+    async def noMemberRole(self, data):
+        # TODO: Docstring...
+
+        # Gather necessary data from the payload.
+        orgEvent, member, message, memberRole, emoji = data
+
+        # Check if user has membership.
+        if memberRole not in member.roles:
+            await member.send(
+                'Sorry, this event is for members only. '
+                'You do not have the clearance to participate or spectate '
+                'this event.',
+                delete_after=60.0
+            )
+            await message.remove_reaction(emoji, member)
+            return True
+        return False
+
+    async def userAlreadyHasRole(self, data):
+        # TODO: docstring.
+
+        # Gather necessary data from payload.
+        orgEvent, member, message, memberRole, emoji = data
+
+        # Check if user already has the role.
+        person = orgEvent.getParticipant(member.id)
+        if person.getRole(orgEvent.roles['participant'].id) is not None:
+            await member.send(
+                'Hmm, you seem to already be registered for the event.'
+                ' Strange. :shrug:',
+                delete_after=60.0
+            )
+            return True
+        return False
+
+    async def addDiscordRole(self, member, role):
+
+        discordRole = member.guild.get_role(role.id)
+        if discordRole not in member.roles:
+            await member.add_roles(discordRole)
+
+    async def sendWelcomeMsg(self, member, channel):
+
+        discordChannel = self.client.get_channel(channel.id)
+        welcomeMsg = (
+            f'Welcome to the event {member.mention}, '
+            'glad to have you!'
+        )
+        await discordChannel.send(welcomeMsg, delete_after=60.0)
+
+    async def gatherPayloadData(self, payload):
+        # Gather necessary data from the payload.
+        messageId = payload.message_id
+        orgEvent = self.getEvent(messageId)
+        channel = self.client.get_channel(payload.channel_id)
+        message = await channel.fetch_message(messageId)
+        guild = message.guild
+        member = guild.get_member(payload.user_id)
+        memberRole = guild.get_role(Constants.MEMBER_ROLE_ID)
+        emoji = payload.emoji.name
+
+        # Return tuple with the needed data.
+        return orgEvent, member, message, memberRole, emoji
+
+    def getEvent(self, id):
         for orgEvent in self.client.orgEvents:
             if orgEvent.id == id:
                 return orgEvent
@@ -174,7 +242,7 @@ class EventSignupHandler(commands.Cog):
         else:
             return False
 
-    def findUser(self, id, orgEvent):
+    def getUser(self, id, orgEvent):
         for user in orgEvent.participants:
             if user.id == id:
                 return user

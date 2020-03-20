db141b8e6089e256a5338f2671c220a23b04532b
ScruffyLNH <anonymousandy829@gmail.com>
2020-03-20 23:28:11 +0100

    Change Id of EVENT_CAT_CHANNEL_ID:
    EVENT_CAT_CHANNEL_ID now is the id of the main event channel, and not a
    separate channel that holds all events. Events are now held in separate
    category channels.

--- constants.py
+++ constants.py
@@ -19,7 +19,7 @@ class Constants:
     MAIN_CHANNEL_ID = 676567548568797266  # TODO: This must be updated to the correct channel.
 
     # ID of the events category channel.
-    EVENTS_CAT_CHANNEL_ID = 688305223164494048
+    EVENTS_CAT_CHANNEL_ID = 676474908603318272
 
     # Discord ID of the bots admin.
     ADMIN_ID = 312381318891700224

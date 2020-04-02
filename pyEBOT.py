269e598b6b072bd9871a20a21845248dc291e2d3
ScruffyLNH <anonymousandy829@gmail.com>
2020-04-02 15:24:35 +0200

    Deserialize data when starting bot.

--- pyEBOT.py
+++ pyEBOT.py
@@ -2,7 +2,9 @@ import discord # noqa
 import os
 import event
 import managedMessages
+from pydantic import ValidationError
 from utility import loadData
+from utility import saveData
 from constants import Constants
 from discord.ext import commands
 
@@ -19,22 +21,51 @@ if __name__ == "__main__":
     # Remove the default help command.
     client.remove_command('help')
 
-    # TODO: Deserialize orgEvent data.
-    # Create empty list that will hold all the event objects
-    client.orgEvents = event.OrgEvents()
-    # Instanciate managedMessages class and store in client.
-    client.managedMessages = managedMessages.ManagedMessages()
-    # client.orgEvents = loadData('eventData.pkl')
-    # if client.orgEvents is None:
-    #     client.orgEvents = []
-    #     print('No record found. Starting clean.')
-    # else:
-    #     # Check if all objects from loaded data are event.Event instances.
-    #     for item in client.orgEvents:
-    #         if not isinstance(item, event.Event):
-    #             client.orgEvents = []
-    #             print('Error in data file. Starting clean.')
-    #             break
+
+    # TODO: Refactor code to deserialize data function.
+    # Deserialize orgEvent data.
+    eventData = loadData('eventData.json')
+    # If eventData.json does not exist client.orgEvents will be
+    # initialized cleanly.
+    if eventData is None:
+        print('No event record found. Starting clean.')
+        client.orgEvents = event.OrgEvents()
+        eventData = client.orgEvents.json(indent=2)
+        saveData('eventData.json')
+    else:
+        try:
+            # Attempt to parse persistent data to orgEvents.
+            client.orgEvents = event.OrgEvents.parse_raw(
+                eventData
+            )
+            print(
+                'Event record successfully parsed.\n'
+                f'Found {len(client.orgEvents)} events.'
+            )
+        except ValidationError as e:
+            print('Record was found, but could not be loaded.')
+            print(e)
+            print('\n Starting clean.')
+            # TODO: Clean up wet code.
+            client.orgEvents = event.OrgEvents()
+            eventData = client.orgEvents.json(indent=2)
+            saveData('eventData.json', eventData)
+
+    messageData = loadData('messageData.json')
+    if messageData is None:
+        print('No message data found.')
+        client.managedMessages = managedMessages.ManagedMessages()
+        messageData = client.managedMessages.json(indent=2)
+        saveData('messageData.json', messageData)
+    else:
+        try:
+            client.managedMessages = managedMessages.ManagedMessages.parse_raw(
+                 messageData
+            )
+            print('Message data successfully parsed.')
+        except ValidationError as e:
+            print('Message record was found, but could not be loaded.')
+            print(e)
 
 
 # Check functions

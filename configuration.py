211ddfa1dcb0da3a997712dd8e632c0cd0eb55be
ScruffyLNH <anonymousandy829@gmail.com>
2020-05-01 07:36:20 +0200

    Create Configuration class.

---
+++ configuration.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class Configuration(BaseModel):
+
+    guildId: int = None
+    mainCategoryChannelId: int = None
+    signupChannelId: int = None
+    discussionChannelId: int = None
+    archiveChannelId: int = None
+    defaultVoiceChannelId: int = None
+    adminChannelId: int = None
+    adminId: int = 312381318891700224
+    eventManagerRoleId: int = None
+    memberRoleId: int = None
+    collaboratorRoleId: int = None

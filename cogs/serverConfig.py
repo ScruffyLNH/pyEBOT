fd51be1f4571a6b5175b326796af9050adde127e
ScruffyLNH <anonymousandy829@gmail.com>
2020-05-04 06:47:43 +0200

    Add logic to serverConfig.

--- cogs/serverConfig.py
+++ cogs/serverConfig.py
@@ -1,7 +1,9 @@
 import discord # noqa
-from utility import saveData
+from utility import saveData, checkConfig, getConfigIds
 from constants import Constants
 from discord.ext import commands
+# TODO: This whole cog should really be rewritten.
+# TODO: Check that guildId has been set before commands are available.
 
 
 class serverConfig(commands.Cog):
@@ -48,7 +50,95 @@ class serverConfig(commands.Cog):
                 return r
         return None
 
-    def processSetChannelRequest(self, channelRef):
+    # Validation checks.
+    async def guildIsSet(self, ctx):
+        if type(self.client.config.guildId) == int:
+            return True
+        else:
+            await ctx.send(
+                'Guild ID must be set before creating or setting any channels '
+                'or roles.'
+            )
+            return False
+
+    async def categoryChannelExists(self, ctx, channelAttribute):
+        if (
+            self.client.config.mainCategoryChannelId is None and
+            'Category' not in channelAttribute
+        ):
+            await ctx.send(
+                'Please set or create the main category channel before '
+                'setting any other channels.'
+            )
+            return False
+        else:
+            return True
+
+    async def attributeFound(self, ctx, attribute):
+        try:
+            getattr(self.client.config, attribute)
+        except AttributeError:
+            await ctx.send(
+                'Could not find the config attribute, make sure the first '
+                'argument is correct.'
+            )
+            return False
+        return True
+
+    async def attrTypeCorrect(self, ctx, attribute, attrType):
+        if attrType in attribute:
+            return True
+        else:
+            await ctx.send(
+                f'Attribute type is not correct for this command.\n'
+                f'Please make sure you are setting a {attrType}'
+            )
+            return False
+
+    async def channelExists(self, ctx, channelRef, expectedResult=False):
+        guild = self.client.get_guild(self.client.config.guildId)
+
+        (channelRef, isInt) = self.tryParseInt(channelRef)
+
+        if isInt:
+            channel = self.verifyChannelId(guild, channelRef)
+        else:
+            channel = self.verifyChannelName(guild, channelRef)
+
+        if expectedResult:
+            if not channel:
+                await ctx.send('Channel not fond.')
+        else:
+            if channel:
+                await ctx.send('Channel already exists.')
+
+        return True if channel else False
+
+    async def roleExists(self, ctx, roleRef, expectedResult=False):
+        guild = self.client.get_guild(self.client.config.guildId)
+
+        (roleRef, isInt) = self.tryParseInt(roleRef)
+
+        if isInt:
+            role = self.verifyRoleId(guild, roleRef)
+        else:
+            role = self.verifyRoleName(guild, roleRef)
+        if expectedResult:
+            if not role:
+                await ctx.send('Role not found.')
+        else:
+            if role:
+                await ctx.send('Role already exists.')
+
+        return True if role else False
+
+    async def serializeConfig(self, ctx, attribute, id):
+        setattr(self.client.config, attribute, id)
+        configData = self.client.config.json(indent=2)
+        saveData(Constants.CONFIG_DATA_FILENAME, configData)
+        await ctx.send(f'{attribute} successfully set, ID is {id}')
+
+    async def processSetChannelRequest(self, channelRef, catChannel=False):  # TODO: Rename?
         # TODO: Docstring
         guild = self.client.get_guild(self.client.config.guildId)
         (channelRef, isInt) = self.tryParseInt(channelRef)
@@ -57,6 +147,17 @@ class serverConfig(commands.Cog):
             channel = self.verifyChannelId(guild, channelRef)
         else:
             channel = self.verifyChannelName(guild, channelRef)
+
+        # Move channel to correct category if not already there.
+        if not catChannel:
+            category = guild.get_channel(
+                self.client.config.mainCategoryChannelId
+            )
+            if channel.category is None:  # TODO: Refactor
+                await channel.edit(category=category)
+            elif category.id != channel.category.id:
+                await channel.edit(category=category)
+
         return channel.id
 
     async def processCreateChannelRequest(
@@ -93,7 +194,7 @@ class serverConfig(commands.Cog):
             role = self.verifyRoleId(guild, roleRef)
         else:
             role = self.verifyRoleName(guild, roleRef)
-        return role
+        return role.id
 
     async def processCreateRoleRequest(self, roleName):
         guild = self.client.get_guild(self.client.config.guildId)
@@ -106,6 +207,8 @@ class serverConfig(commands.Cog):
     # Commands
     @commands.command(name='setGuildId')
     async def setGuildId(self, ctx):
+        """Sets the ID of the current guild as the event bot guild ID.
+        """
         # Get the id of the guild from where the command was invoked.
         guildId = ctx.guild.id
         # Set the id in config object
@@ -118,36 +221,159 @@ class serverConfig(commands.Cog):
 
     @commands.group(name='configureChannels', invoke_without_command=True)
     async def configChn(self, ctx):
+        """Command group for configuring channels. Use help command for sub commands.
+        """
         await ctx.send(
-            'The following subcommands are available:\n'
-            'setMainCategory (param: channelName or Id) \n'
-            'createMainCategory (param: ChannelName)\n'
+            'This is a command group. Use help function for subcommands\n'
+            f'{Constants.CMD_PREFIX}help configureChannels'
         )
 
-    @configChn.command(name='setMainCategory')
-    async def setMainCategory(self, ctx, channelRef):
-        id = self.processSetChannelRequest(channelRef)
-        if id is None:
+    @configChn.command(name='setChannel')
+    async def setChannel(self, ctx, channelAttribute, discordChannel):
+        """Link existing discord channels to config channels.
+
+        :param channelAttribute: The Config attribute. Use checkConfiguration
+        command to see attributes yet to be configured.
+        :type channelAttribute: string
+        :param discordChannel: Name/id of discord channel to be set in config.
+        :type discordChannel: string
+        """
+        # Do verification checks
+        if not await self.guildIsSet(ctx):
+            return
+
+        if not await self.categoryChannelExists(ctx, channelAttribute):
+            return
+
+        if not await self.channelExists(
+            ctx, discordChannel, expectedResult=True
+        ):
+            return
+
+        if not await self.attributeFound(ctx, channelAttribute):
+            return
+
+        if not await self.attrTypeCorrect(ctx, channelAttribute, 'Channel'):
+            return
+
+        # Get id of the discord channel if it exists.
+        channelId = await self.processSetChannelRequest(discordChannel, True)
+
+        if channelId is None:
             await ctx.send('Could not find the requested channel.')
         else:
-            self.client.config.mainCategoryChannelId = id
-            configData = self.client.config.json(indent=2)
-            saveData(Constants.CONFIG_DATA_FILENAME, configData)
-            await ctx.send(f'Category channel with id {id} set successfully.')
+            await self.serializeConfig(ctx, channelAttribute, channelId)
 
-    @configChn.command(name='createMainCategory')
-    async def createMainCategory(self, ctx, channelName):
-        id = await self.processCreateChannelRequest(
-            channelName,
-            channelType='category'
-        )
-        if id is None:
-            await ctx.send('Could not create channel. Check if name is taken.')
+    @configChn.command(name='createChannel')
+    async def createChannel(self, ctx, channelAttribute, discordChannel):
+        """Create discord channel and link to config.
+
+        :param channelAttribute: The config attribute use checkConfiguration
+        command to see attributes yet to be configured.
+        :type channelAttribute: string
+        :param discordChannel: Name of the discord channel
+        :type discordChannel: string
+        """
+
+        # Do verification checks
+        if not await self.guildIsSet(ctx):
+            return
+
+        if not await self.categoryChannelExists(ctx, channelAttribute):
+            return
+
+        if await self.channelExists(ctx, discordChannel):
+            return
+
+        if not await self.attributeFound(ctx, channelAttribute):
+            return
+
+        if not await self.attrTypeCorrect(ctx, channelAttribute, 'Channel'):
+            return
+
+        # Determine the channel type.
+        if 'Voice' in channelAttribute:
+            channelType = 'voice'
+        elif 'Category' in channelAttribute:
+            channelType = 'category'
         else:
-            self.client.config.mainCategoryChannelId = id
-            configData = self.client.config.json(indent=2)
-            saveData(Constants.CONFIG_DATA_FILENAME, configData)
-            await ctx.send(f'Channel created successfully. ID is {id}')
+            channelType = 'text'
+
+        channelId = await self.processCreateChannelRequest(
+            discordChannel,
+            channelType=channelType
+        )
+
+        await self.serializeConfig(ctx, channelAttribute, channelId)
+
+    @commands.group(name='configureRoles', invoke_without_command=True)  # TODO: Check that guildId has been set.
+    async def configRole(self, ctx):
+        """Command group for configuring roles. Use help command for sub commands.
+        """
+        await ctx.send(
+            'This is a command group. Use help function for subcommands\n'
+            f'{Constants.CMD_PREFIX}help configureRoles'
+        )
+
+    @configRole.command(name='setRole')
+    async def setRole(self, ctx, roleAttribute, discordRole):
+
+        # Do verification checks.
+        if not await self.guildIsSet(ctx):
+            return
+        if not await self.roleExists(ctx, discordRole, expectedResult=True):
+            return
+        if not await self.attributeFound(ctx, roleAttribute):
+            return
+        if not await self.attrTypeCorrect(ctx, roleAttribute, 'Role'):
+            return
+
+        roleId = self.processSetRoleRequest(discordRole)
+        await self.serializeConfig(ctx, roleAttribute, roleId)
+
+    @configRole.command(name='createRole')
+    async def createRole(self, ctx, roleAttribute, discordRole):
+
+        # Do verification checks.
+        if not await self.guildIsSet(ctx):
+            return
+        if await self.roleExists(self, ctx, discordRole):
+            return
+        if await self.attributeFound(ctx, roleAttribute):
+            return
+        if not await self.attrTypeCorrect(ctx, roleAttribute, 'Role'):
+            return
+
+        roleId = self.processCreateRoleRequest(discordRole)
+
+        await self.serializeConfig(ctx, roleAttribute, roleId)
+
+    @commands.command(name='checkConfiguration', aliases=['checkConfig'])
+    async def checkConfiguration(self, ctx):
+        """Check which configuration fields needs to be configured.
+        """
+        remainingFields = '\n'.join(checkConfig(self.client.config))
+        completedFields = '\n'.join(getConfigIds(self.client.config))
+
+        # Build the message with a buffer
+        buffer = []
+
+        if remainingFields:
+            buffer.append(
+                'Remaining fields to be configured are as follows:\n'
+            )
+            buffer.append(remainingFields)
+            buffer.append('\n\n')
+        else:
+            buffer.append('Configuration is complete\n\n')
+
+        if completedFields:
+            buffer.append(
+                'The following fields have been configured:\n'
+            )
+            buffer.append(completedFields)
+
+        await ctx.send(''.join(buffer))
 
     # Command check for entire cog.
     def cog_check(self, ctx):

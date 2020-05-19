2c280d0b748f6c876e18fc6731496306e30540f8
ScruffyLNH <anonymousandy829@gmail.com>
2020-05-19 21:14:00 +0200

    update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,5 +1,6 @@
 import gspread  # Module needed for google sheets.
 import csv
+import string
 from oauth2client.service_account import ServiceAccountCredentials
 # from pprint import pprint  # Print pretty messages.
 
@@ -28,17 +29,33 @@ data = sheet.get_all_records()
 
 pprint(data)
 """
-
+# TODO: Do some error handling to prevent crashing if sheet names change.
 spreadSheet = client.open("EVENTS")
 sheet = spreadSheet.worksheet('Discord Parsed Data')
-# TODO: Get the correct daymar sheet
+
 daymarSpreadSheet = client.open_by_key(
-    '1oRsiSXzeJKMczrEZtEhDLcnC_tPNjtLD7n3MqftfZbc'
+    '1VDhj5Fwoo4auUMBc3bJ8KzVjpCPcCwXnwcVKs9GMRDQ'
 )
 daymarSheet = daymarSpreadSheet.worksheet('SECURITY')
+daymarOverviewSheet = daymarSpreadSheet.worksheet('ALL')
 
 
 # Functions
+def numToA(n, b=string.ascii_uppercase):
+    """Converts number to column letter notation. (Zero indexed.)
+
+    :param n: Number to convert
+    :type n: int
+    :param b: Letters to convert to. Defaults to string.ascii_uppercase
+    :type b: string constant, optional
+    :return: The column letter notation of the input.
+    :rtype: str
+    """
+
+    d, m = divmod(n, len(b))
+    return numToA(d-1, b) + b[m] if d else b[m]
+
+
 def getCell(rowIndex, colIndex, daymar=False):
     if daymar:
         cell = daymarSheet.cell(rowIndex, colIndex)
@@ -54,10 +71,25 @@ def setCell(rowIndex, colIndex, value, daymar=False):
         sheet.update_cell(rowIndex, colIndex, value)
 
 
-def getAll(daymar=False):
+def setRange(startIndex, endIndex, data, daymar=False):
+
+    # Convert indices to A1 notation.
+    start = numToA(startIndex[1]) + str(startIndex[0] + 1)
+    end = numToA(endIndex[1]) + str(endIndex[0] + 1)
+    a1Range = ':'.join([start, end])
+
+    if daymar:
+        daymarSheet.update(a1Range, data)
+    else:
+        sheet.update(a1Range, data)
+
+
+def getAll(daymar=False, daymarOverview=False):
 
     if daymar:
         return daymarSheet.get_all_records()
+    elif daymarOverview:
+        return daymarOverviewSheet.get_all_records()
     else:
         return sheet.get_all_records()
 
@@ -96,7 +128,7 @@ def exportCsv(fileName):
 
     with open(fileName, 'w', newline='') as fp:
         writer = csv.writer(fp)
-        writer.writerows(daymarSheet.get_all_values())
+        writer.writerows(daymarOverviewSheet.get_all_values())
 
         return fp
 
